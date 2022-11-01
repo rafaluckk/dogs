fd7acdf19db9b676ea1404c95126c5d55acb2ebf
rafaluckk <rafael2021516@gmail.com>
2022-11-01 16:50:24 -0300

    Header inicio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-
-
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
     <div>
-
-      
+      <BrowserRouter>
+          <Header />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/login" element={<Login />}/>
+        </Routes>
+          <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Footer.modules.css'
+
+
+const Footer = () => {
+  return (
+    <div className={styles.Footer}>Footer
+      
+    </div>
+  )
+}
+
+export default Footer
